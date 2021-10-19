0224a28cadbb067bb6e5a514d14b405a60dea132
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-10-19 22:09:21 +0200

    Multiples

--- PrikladyZNetu/Program.cs
+++ PrikladyZNetu/Program.cs
@@ -6,10 +6,24 @@ namespace PrikladyZNetu
     {
         static void Main(string[] args)
         {
-            FindTheBomb findbomb = new FindTheBomb();
-            Console.WriteLine("Write the sentense:");
-            string reseni = findbomb.FindBomb(Console.ReadLine());
-            Console.WriteLine(reseni);
+
+            Console.WriteLine("Zadej délku pole:");
+            int delkaPole = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej číslo, které chceš mít znásobeno v poli:");
+            int nasobek = int.Parse(Console.ReadLine());
+            int[] pole = new int[delkaPole];
+
+            for (int i = 0; i < delkaPole; i++)
+            {
+                pole[i] = (nasobek + nasobek);
+                nasobek = nasobek + nasobek;
+            }
+
+            foreach (int i in pole)
+            {
+                Console.WriteLine("{0}", i);
+            }
+
         }
     }
 }
