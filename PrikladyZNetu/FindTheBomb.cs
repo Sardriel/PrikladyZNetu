3518b9ac09b2e800259d4224a135b79131228bb4
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-10-19 19:08:48 +0200

    Add project files.

---
+++ PrikladyZNetu/FindTheBomb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrikladyZNetu
+{
+    class FindTheBomb
+    {
+        public string FindBomb(string sentense)
+        {
+            string bomb = "Duck!!!";
+            string nobomb = "Relax, there is no bomb.";
+            sentense = sentense.ToLower();
+            if (sentense.Contains("bomb"))
+            {
+                return bomb;
+            }
+            else
+            {
+                return nobomb;
+            }
+        }
+
+
+    }
+}
---
+++ PrikladyZNetu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrikladyZNetu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindTheBomb findbomb = new FindTheBomb();
+            Console.WriteLine("Write the sentense:");
+            string reseni = findbomb.FindBomb(Console.ReadLine());
+            Console.WriteLine(reseni);
+        }
+    }
+}
